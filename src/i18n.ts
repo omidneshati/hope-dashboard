f1783e1b43175ac138c04dd8bd4deddec5bbcecf
Omid-Neshati <tchandnofame@gmail.com>
2023-01-06 16:00:56 +0330

    add translation files path and types

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+import Backend, { HttpBackendOptions } from 'i18next-http-backend';
 
 i18n
   // i18next-http-backend
@@ -15,15 +15,19 @@ i18n
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
+  .init<HttpBackendOptions>({
     debug: true,
     fallbackLng: 'en',
-    resources: {
-      en: {
-        translation: {
-          // here we will place our translations...
-        }
-      }
+    // resources: {
+    //   en: {
+    //     translation: {
+    //       // here we will place our translations...
+    //     }
+    //   }
+    // },
+    backend: {
+      // for all available options read the backend's repository readme file
+      loadPath: '/locales/{{lng}}/translation.json'
     }
   });
 
