456b74174f07488a068ce853bd5255560fbdc777
Omid-Neshati <tchandnofame@gmail.com>
2023-01-06 21:40:37 +0330

    add react-router-dom & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
 import TestComp from './components/TestComp';
+import Home from './pages/home';
+import Customer from './pages/Customer';
+import Inventory from './pages/Inventory';
+import Order from './pages/Order';
+import Report from './pages/Report';
+import Page404 from './pages/Page404';
 
 function App() {
   return (
     <main>
-      <TestComp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="customer" element={<Customer />} />
+          <Route path="inventory" element={<Inventory />} />
+          <Route path="order" element={<Order />} />
+          <Route path="report" element={<Report />} />
+          <Route path="test" element={<TestComp />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
