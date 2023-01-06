b9b75b0134bf35a21e0e86e267bab48234d4a84e
Omid-Neshati <tchandnofame@gmail.com>
2023-01-06 16:01:53 +0330

    translation worked

--- src/components/TestComp/index.tsx
+++ src/components/TestComp/index.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import { changeLanguage } from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 import style from './TestComp.module.css';
 
 function TestComp() {
   const [num, setNum] = useState(0);
+  const { t } = useTranslation();
 
   const increaseNum = () => setNum((v) => v + 1);
+
+  const langs = ['en', 'fa'];
 
   return (
     <div>
@@ -14,7 +19,23 @@ function TestComp() {
       <button type="button" onClick={increaseNum}>
         Click ME
       </button>
-      <p className={style.testWritMode}>Omid Neshati</p>
+      <div className="border-2 border-black space-x-1 p-5">
+        {langs.map((lang) => {
+          return (
+            <button
+              className="p-1 border border-cyan-900"
+              key={lang}
+              type="button"
+              onClick={() => changeLanguage(lang)}>
+              {lang}
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        <i>{t('key')}</i>
+        <span className={style.testWritMode}>Omid Neshati</span>
+      </div>
     </div>
   );
 }
