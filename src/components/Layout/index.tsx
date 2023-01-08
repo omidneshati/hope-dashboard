2e7d6eaec41780a511b455ee14c6533cba00b543
Omid-Neshati <tchandnofame@gmail.com>
2023-01-08 22:05:54 +0330

    add sidebar to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,22 @@
 import { Provider } from 'react-redux';
 import { store } from '@redux/store';
 import { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Sidebar from '../Sidebar';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <main>
+          <header>header</header>
+          <div>
+            <Sidebar />
+          </div>
+          {children}
+          <footer>footer</footer>
+        </main>
+      </BrowserRouter>
+    </Provider>
+  );
 }
