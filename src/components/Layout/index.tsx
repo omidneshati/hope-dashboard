be12423533d7849d8faf85b5da42ad0c9b358f4b
Omid-Neshati <tchandnofame@gmail.com>
2023-01-08 21:34:01 +0330

    add sidebarReducer to store

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,7 @@
+import { Provider } from 'react-redux';
+import { store } from '@redux/store';
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
