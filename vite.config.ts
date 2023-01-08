f19b2dd27035eeed4df3d4b8eb24c58d1c2bb900
Omid Neshati <tchandnofame@gmail.com>
2023-01-08 22:18:11 +0330

    fix alias src path

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
