63900bdd8184eda95f700617eb5e47334b62553e
Omid-Neshati <tchandnofame@gmail.com>
2023-01-09 21:23:44 +0330

    solve eslint import/extension error

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,8 @@
 import { Provider } from 'react-redux';
 import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from '../../redux/store';
+// import { store } from '../../redux/store';
+import { store } from '@/redux/store';
 import Sidebar from '../Sidebar';
 
 export default function Layout({ children }: { children: ReactNode }) {
