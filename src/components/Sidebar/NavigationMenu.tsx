fd87885c8a2cea386b11e1be3b96d4f19eaf1ae7
Omid-Neshati <tchandnofame@gmail.com>
2023-01-23 05:56:18 +0330

    change style and wrap component in fragment

--- src/components/Sidebar/NavigationMenu.tsx
+++ src/components/Sidebar/NavigationMenu.tsx
@@ -30,24 +30,22 @@ const pages: { link: string; title: string; icon: IconType }[] = [
 
 export default function NavigationMenu() {
   return (
-    <nav className="flex flex-col gap-2 flex-1 mt-6">
+    <>
       {pages.map((page) => {
         return (
           <NavLink
             key={page.title}
             to={page.link}
             className={({ isActive }) =>
-              `flex items-center px-4 py-2 text-gray-700 ${
-                isActive
-                  ? 'bg-gray-100 dark:bg-gray-800'
-                  : 'border border-gray-300 dark:border-gray-600'
-              } rounded-md  dark:text-gray-200`
+              `flex items-center px-4 py-2 text-gray-700 border-gray-700 ${
+                isActive && 'border-ie-4'
+              }`
             }>
             <page.icon className="w-5 h-5" />
             <span className="mx-4 font-medium">{page.title}</span>
           </NavLink>
         );
       })}
-    </nav>
+    </>
   );
 }
